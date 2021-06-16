accaea1d82921b9f7608b7faa988d4a9b508776f
Cristian <cristianmesa13@gmail.com>
2021-06-16 17:30:56 -0300

    se termino con las tareas de formulario,login,logout totalmente funcional

--- social/views.py
+++ social/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from .forms import UserRegisterForm
+
+from django.contrib import messages
 
 
 # Create your views here.
@@ -7,6 +10,21 @@ def feed(request):
     posts=Post.objects.all()
     context={'posts': posts}
     return render(request, 'social/feed.html',context)
+def register(request):
+    # El if es para utilizar los campos que fueron llenados en el formulario
+    # El else es para que se muestre el formulario vacio
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'Usuario {username} creado')
+            return redirect('feed')
+    else:
+        form = UserRegisterForm() 
+    context={'form': form}           
+
+    return render(request, 'social/register.html', context)    
 
 def profile(request):
     return render(request,'social/profile.html')
